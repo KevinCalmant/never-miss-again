674872680123d55e00e8dbda555036070e20d212
Kévin Calmant <kevin.calmant@outlook.com>
2026-05-03 20:24:43 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'never-miss-again',
+  webDir: 'www'
+};
+
+export default config;
